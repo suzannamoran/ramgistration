d5d5917c19decb16aef025491df737e84ea5ec8b
Nicole Arwood <nsarwood@gmail.com>
2023-02-11 14:24:13 -0500

    Sample scraping method

--- nicole.py
+++ nicole.py
@@ -1,1 +1,22 @@
+<<<<<<< HEAD
 #heyyyyyyyyyyyyyy
+=======
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://catalog.unc.edu/undergraduate/programs-study/"
+data = requests.get(url)
+html = data.text
+soup = BeautifulSoup(html, 'html.parser')
+
+def main():
+    majors = []
+    minors = []
+    links = soup.find_all("li")
+    for link in links:
+        print(link.get('a href'))
+        print("\n")
+
+if __name__ == "__main__":
+    main()
+>>>>>>> c49a49d (for zan)
